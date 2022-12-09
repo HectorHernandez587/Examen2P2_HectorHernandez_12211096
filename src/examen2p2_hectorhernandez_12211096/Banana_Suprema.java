8db3124eecbcf635f7b0d1964ce69dfdc7fd655f
HectorHernandez587 <eduhp587@gmail.com>
2022-12-09 17:56:57 -0600

    Avance 9

---
+++ src/examen2p2_hectorhernandez_12211096/Banana_Suprema.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2p2_hectorhernandez_12211096;
+
+/**
+ *
+ * @author Hector
+ */
+public class Banana_Suprema {
+    private int x;
+    private int y;
+
+    public Banana_Suprema() {
+    }
+
+    public Banana_Suprema(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Banana_Suprema{" + "x=" + x + ", y=" + y + '}';
+    }
+    
+    
+    
+}
--- src/examen2p2_hectorhernandez_12211096/Hilo.java
+++ src/examen2p2_hectorhernandez_12211096/Hilo.java
@@ -14,19 +14,22 @@ import java.util.logging.Logger;
  * @author Hector
  */
 public class Hilo implements Runnable {
+
     Random r = new Random();
 
-    private String [][] matriz;
+    private String[][] matriz;
     private String instrucciones;
     private String bananas;
+    private Banana_Suprema bs;
 
     public Hilo() {
     }
 
-    public Hilo(String[][] matriz, String instrucciones, String bananas) {
+    public Hilo(String[][] matriz, String instrucciones, String bananas, Banana_Suprema bs) {
         this.matriz = matriz;
         this.instrucciones = instrucciones;
         this.bananas = bananas;
+        this.bs = bs;
     }
 
     public String[][] getMatriz() {
@@ -53,66 +56,73 @@ public class Hilo implements Runnable {
         this.bananas = bananas;
     }
 
+    public Banana_Suprema getBs() {
+        return bs;
+    }
+
+    public void setBs(Banana_Suprema bs) {
+        this.bs = bs;
+    }
+
     @Override
     public String toString() {
-        return "Hilo{" + "matriz=" + matriz + ", instrucciones=" + instrucciones + ", bananas=" + bananas + '}';
+        return "Hilo{" + "matriz=" + matriz + ", instrucciones=" + instrucciones + ", bananas=" + bananas + ", bs=" + bs + '}';
     }
 
     @Override
     public void run() {
+
         int largo = 0;
         if (largo < instrucciones.length()) {
             String ultimo = instrucciones.substring(instrucciones.length() - 1);
             char[] lastChar = ultimo.toCharArray();
             //int coor_x = 1 + r.nextInt(11);
             //int coor_y = 1 + r.nextInt(31);
-            int coor_x = 5;
-            int coor_y = 5;
-            
-            matriz[coor_x][coor_y] = "@";
-            matriz[coor_x - 1][coor_y] = "X";
-            matriz[coor_x - 2][coor_y] = "X";
-            
+            int coor_x = bs.getX();
+            int coor_y = bs.getY();
 
-            if (lastChar[lastChar.length - 1] == 'W' || lastChar[lastChar.length - 1] == 'w') {
-                try {
-                    //Arriba
-                    
+            while (true) {
+                if (lastChar[lastChar.length - 1] == 'W' || lastChar[lastChar.length - 1] == 'w') {
+                    try {
+                        //Arriba
+                        bs.setY(coor_y - 1);
+                        coor_y = bs.getY();
 
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
 
-            } else if (lastChar[lastChar.length - 1] == 'S' || lastChar[lastChar.length - 1] == 's') {
-                try {
-                    //Abajo
+                    }
 
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                } else if (lastChar[lastChar.length - 1] == 'S' || lastChar[lastChar.length - 1] == 's') {
+                    try {
+                        //Abajo
 
-            } else if (lastChar[lastChar.length - 1] == 'A' || lastChar[lastChar.length - 1] == 'a') {
-                try {
-                    //Izquierda
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
 
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                    }
 
-            } else if (lastChar[lastChar.length - 1] == 'D' || lastChar[lastChar.length - 1] == 'd') {
-                try {
-                    //Derecha
+                } else if (lastChar[lastChar.length - 1] == 'A' || lastChar[lastChar.length - 1] == 'a') {
+                    try {
+                        //Izquierda
 
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+
+                    }
+
+                } else if (lastChar[lastChar.length - 1] == 'D' || lastChar[lastChar.length - 1] == 'd') {
+                    try {
+                        //Derecha
+
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+
+                    }
+
                 }
 
             }
-
         }
 
     }
